e92e2c1925a25515bb0c2fa418b4092914bef978
Davide Di Girolamo <davide.digi2001@gmail.com>
2022-12-22 18:55:57 +0100

    fine esercizio mail  bonus

--- mail/Bonus/script.js
+++ mail/Bonus/script.js
@@ -1,18 +1,30 @@
-const askMail = prompt('Inserisci la tua email:');
-
+const eMail = document.getElementById("email");
+const button = document.getElementById('button-addon2');
+const main = document.querySelector("main");
 const usersList = ['davide@gmail.com', 'lidia@tiscali.it', 'fabio@hotmail.com', 'antonio@gmail.com', 'nicole@alice.it'];
 
-console.log(usersList);
-
 let i = 0;
+button.addEventListener('click',
 
-for (i; i < usersList.length; i++) {
-    
-    if (askMail == usersList[i]) {
-        alert('accesso consentito')
-        break;
+    function() {
+        for (i; i < usersList.length; i++) {
+            
+            if (eMail.value == usersList[i]) {
+
+                const newElement = document.createElement('div');
+                newElement.innerHTML = 'Accesso consentito, la tua email è corretta!';
+                main.append(newElement);
+                newElement.classList.add("bg-light","w-25","mx-auto","p-3","mt-4","rounded","text-center","text-success")
+                
+                break;
+            }
+        }
+        if (eMail.value != usersList[i]) {
+
+            const newnewElement = document.createElement('div');
+            newnewElement.innerHTML = 'Accesso negato, la tua email non è registrata!';
+            main.append(newnewElement);
+            newnewElement.classList.add("bg-light","w-25","mx-auto","p-3","mt-4","rounded","text-center", "text-danger")
+        }
     }
-}
-if (askMail != usersList[i]) {
-    alert('accesso negato, la tua email non è registrata')
-}
+);
