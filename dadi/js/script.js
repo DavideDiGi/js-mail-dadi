d08acfce4b2ea8a0f19091f0296fcc0529396e8e
Davide Di Girolamo <davide.digi2001@gmail.com>
2022-12-23 09:58:53 +0100

    aggiunto pareggio

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,11 +1,15 @@
-let player = Math.random();
-console.log('player: ', Math.floor(player * 6) + 1);
-let computer = Math.random();
-console.log('computer: ', Math.floor(computer * 6) + 1);
+let player = Math.floor(Math.random() * 6 + 1);
+let computer = Math.floor(Math.random() * 6 + 1);
+
+console.log('player: ', player, typeof player);
+console.log('computer: ', computer, typeof computer);
 
 if (player > computer) {
     console.log('il vincitore è: player!');
 }
 else if (player < computer) {
     console.log('il vincitore è: computer!');
+}
+else{
+    console.log('pareggio!');
 }
